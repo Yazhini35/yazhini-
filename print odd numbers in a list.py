4ae71f8e2b6e9402e646950dd4cc023bda9c099a
Yazhini35 <120041954+Yazhini35@users.noreply.github.com>
2022-12-13 19:10:19 +0530

    Add files via upload

---
+++ print odd numbers in a list.py
@@ -0,0 +1,5 @@
+list=[1,2,3,4,9,7,5]
+for num in list:
+    if num%2!=0:
+     print(num,end="")
+
---
+++ sum,sub,multiply in min and max.py
@@ -0,0 +1,9 @@
+array=[56,89,5,1,68]
+print("The maximum number is,",max(array))
+print("The minimum number is,",min(array))
+sum=max+min
+difference=max-min
+product=max*min
+print("sum is,",sum)
+print("difference is,",difference)
+print("product is,",product)
