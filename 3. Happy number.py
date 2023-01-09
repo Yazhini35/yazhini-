57f8908c6cdbc04285abb59895641123a1304a7b
Yazhini35 <120041954+Yazhini35@users.noreply.github.com>
2023-01-09 20:50:15 +0530

    Add files via upload

---
+++ 3. Happy number.py
@@ -0,0 +1,12 @@
+def happy(n):
+  present=set()
+  while n!=1:
+        n= sum(int(i)**2)
+               for i in str(n)
+        if n in present:
+            return false
+        present.add(n)
+  return type
+print(happynum(7))
+print(happynum(834))
+print(happynum(6))
